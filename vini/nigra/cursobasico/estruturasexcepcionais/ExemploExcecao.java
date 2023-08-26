7c33727d512996e6a7323b5127d9bd2e0edd7f8d
Vinicius Augusto Nigra <111812339+vininigra@users.noreply.github.com>
2023-08-26 17:19:49 -0300

    tratando excecoes

---
+++ vini/nigra/cursobasico/estruturasexcepcionais/ExemploExcecao.java
@@ -0,0 +1,8 @@
+package vini.nigra.cursobasico.estruturasexcepcionais;
+
+public class ExemploExcecao {
+    public static void main(String[] args) {
+        Number valor = Double.valueOf("a1.75");
+        System.out.println(valor);
+    }
+}
