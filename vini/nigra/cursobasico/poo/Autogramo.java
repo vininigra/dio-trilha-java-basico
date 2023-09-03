8606731629efb057c34923f91fb79dcc72e6006a
Vinicius Augusto Nigra <111812339+vininigra@users.noreply.github.com>
2023-09-03 20:58:34 -0300

    Pilares Do POO

---
+++ vini/nigra/cursobasico/poo/Autogramo.java
@@ -0,0 +1,8 @@
+public class Autogramo {
+    public static void main(String[] args) {
+        Carro jeep = new Carro();
+        jeep.confereCambio();
+        jeep.confereCombustivel();
+        jeep.ligar();
+    }
+}
---
+++ vini/nigra/cursobasico/poo/Carro.java
@@ -0,0 +1,14 @@
+public class Carro {
+
+        public void ligar(){
+            System.out.println("Carro Ligado");
+        }
+        public void confereCombustivel(){
+            System.out.println("Confere Combustivel");
+        }
+        public void confereCambio(){
+            System.out.println("Confere Cambio");
+        }
+    }
+
+
